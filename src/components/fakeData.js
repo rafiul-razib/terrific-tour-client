2be22230886c6917a2cbbbdd02ee08dbaa806d33
rafiul-razib <rafiul.rzb@gmail.com>
2021-11-21 23:02:14 +0600

    done

---
+++ src/components/fakeData.js
@@ -0,0 +1,37 @@
+const images = [
+    {   id: 1,
+        img: "https://i.ibb.co/cr5K8Mk/meritt-thomas-Sx-H084-Wv2-Jk-unsplash.jpg"
+    },
+    {
+        id:2,
+        img: "https://i.ibb.co/MMbQFFP/nico-smit-8-Rk1-Dq-L-Lkc-unsplash.jpg"
+    },
+    {
+        id:3,
+        img: "https://i.ibb.co/zffFNNB/isaw-company-vqr-LASCWJ-o-unsplash.jpg"
+    },
+    {
+        id:3,
+        img: "https://i.ibb.co/xCTw1N4/stephan-valentin-Vwd6a-R5o41-Q-unsplash.jpg"
+    },
+    {
+        id:4,
+        img: "https://i.ibb.co/NswnHG0/siebe-warmoeskerken-mx-Nrt-Fz-Od-I-unsplash.jpg"
+    },
+    {
+        id:4,
+        img: "https://i.ibb.co/4jQ05fx/saikrishna-saketh-yellapragada-y-Befb-Itr7i-U-unsplash.jpg"
+    },
+    {
+        id:5,
+        img: "https://i.ibb.co/vYtFG6m/kathy-marsh-klmp1z-Pklfc-unsplash.jpg"
+    },
+    {
+        id:6,
+        img: "https://i.ibb.co/7XSFh2h/alistair-boyd-f-R9-Oig-V3-VLY-unsplash.jpg"
+    },
+    {
+        id:7,
+        img: "https://i.ibb.co/vYtFG6m/kathy-marsh-klmp1z-Pklfc-unsplash.jpg"
+    }
+]
